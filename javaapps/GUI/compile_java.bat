0fb7df962e0d34fd2e5864905c821348755de7af
Nick Larkin <psychowolf94@gmail.com>
2026-07-21 16:02:55 -0600

    fix: recompile all Java class files targeting Java 8 bytecode for CheerpJ runtime compatibility

--- javaapps/GUI/compile_java.bat
+++ javaapps/GUI/compile_java.bat
@@ -1,6 +1,6 @@
 @echo off
-echo Compiling all Java files in GUI folder...
-javac *.java
+echo Compiling all Java files in GUI folder with Java 8 compatibility for CheerpJ Web JVM...
+javac --release 8 *.java
 if %errorlevel% neq 0 (
     echo Compilation failed with errors!
     exit /b %errorlevel%
